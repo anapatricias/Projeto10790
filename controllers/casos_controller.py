e4136177d57013b2457c54a8301bdcdd04571f69
anapatricias <anapatriciaportelinha@gmail.com>
2025-07-30 23:14:03 +0100

    1

--- controllers/casos_controller.py
+++
@@ -1,8 +0,0 @@
-casos = []
-
-def guardar_caso(caso):
-    casos.append(caso)
-    print(f"Caso {caso.id} guardado com sucesso!")
-
-def listar_casos_por_autor(autor):
-    return [caso for caso in casos if caso.autor == autor]
